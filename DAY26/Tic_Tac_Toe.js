fee959ecc71631b901027113f2bfac7a0a45554a
Vector-mayu <mayureshdandekar143@gmail.com>
2025-11-01 17:19:16 +0530

    Uploaded Tic Tac Toe Final Project

--- DAY26/Tic_Tac_Toe.js
+++ DAY26/Tic_Tac_Toe.js
@@ -1,13 +1,28 @@
-const board = document.querySelector('.board');
-let turn = 'O';
+// Winner Array 
 
-let winner = [
+// let winner = [
+//   [0, 1, 2], top row
+//   [3, 4, 5], middle row
+//   [6, 7, 8], bottom row
+//   [0, 3, 6], left column
+//   [1, 4, 7], middle column
+//   [2, 5, 8], right column
+//   [0, 4, 8], main diagonal
+//   [2, 4, 6]  anti diagonal
+// ];
+
+
+const board = document.querySelector('.board'); // selecting the element from HTML
+let turn = 'O'; // 1st Turn should be for O
+let total_turns = 0;
+
+const winner = [
 	[0,1,2],[3,4,5],[6,7,8],
 	[0,3,6],[1,4,7],[2,5,8],
 	[0,4,8],[2,4,6]
-]
+] // 2-D Array for winning Condition
 
-let tictactoe = new Array(9).fill('E');
+let tictactoe = new Array(9).fill('E'); // Create an array of size 9 and initially fill all index with E
 
 function CheckWinner(){
 	for(let [index0, index1, index2] of winner)
@@ -21,30 +36,64 @@ function CheckWinner(){
 	return 0;
 }
 
-board.addEventListener('click', (event) => {
+let callgame = (event) => {
 	const element = event.target;
 	
 	if(tictactoe[element.id] === 'E')
 	{
+		total_turns++;
 		if(turn === 'O')
 		{
 			element.innerText = 'O';
 			tictactoe[element.id] = 'O';
+			element.style.color = 'rgb(68, 137, 240)';
 			if(CheckWinner())
 			{
-				document.querySelector('.winning-message').innerHTML = "Winner O"
+				let msg = document.querySelector('.winning-message');
+				msg.style.color = 'rgb(68, 137, 240)';
+				msg.innerText = `Winner O`;
+				board.removeEventListener('click', callgame);
+				return;
 			}
 			turn = 'X';
 		}
 		else{
 			element.innerText = 'X';
 			tictactoe[element.id] = 'X'
+			element.style.color = 'rgba(243, 60, 63, 1)';
 			if(CheckWinner())
 			{
-				document.querySelector('.winning-message').innerHTML = "Winner X";
+				let msg = document.querySelector('.winning-message');
+				msg.style.color = 'rgba(243, 60, 63, 1)';
+				msg.innerText = `Winner X`;
+				board.removeEventListener('click', callgame);
+				return;
 			}
 			turn = 'O';
 		}
 	}
-});
+
+	if(total_turns === 9)
+	{
+		document.querySelector('.winning-message').innerHTML = `Match Draw`;
+	}
+}
+
+board.addEventListener('click', callgame);
+
+let Restart = document.getElementById('restartButton');
+Restart.addEventListener('click', (event) => {
+	const cell = document.getElementsByClassName('cell');
+
+	Array.from(cell).forEach((value)=>{
+		value.innerHTML = '';
+	})
+
+	turn = 'O';
+	total_turns = 0;
+	tictactoe = new Array(9).fill('E');
+	board.addEventListener('click', callgame);
+	document.getElementById('winningMessage').innerHTML = ' ';
+})
+
 
--- DAY26/readme.md
+++ DAY26/readme.md
@@ -3,3 +3,20 @@ Step 1 : Simple Printing of X and O
 -> Add Event Listner to 
 -> add innerHTML in from of if else condition
 -> Create Turn starting with O in the game
+
+// Steps For JS Code
+
+-> select the board
+-> create a winner array consisting index of winnning condition
+-> create a array which should store the info of the cell's status (E, O, X)
+
+-> create a function to check winner 
+-> add event listner to the board which references to the exact HTML element that triggered the event
+-> Check (if-else) for X or O 
+-> Keep Checking the winner condtion after each operation of (O/X)
+-> if its a winning condition then display the winning message 
+
+-> also stop when there is a winner 
+	-> use .removeEventListner to stop when there is a winner
+	-> creae a separate func for the main execution
+
